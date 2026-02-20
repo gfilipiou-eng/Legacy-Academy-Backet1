c97b9478f7a9e8b191b71d62bf65cc86313dedda
Filip Posta <bagugan2009@gmail.com>
2026-02-20 13:06:03 +0100

    Fix: Suppress all console logs in production explicitly

--- legacy-academy-frontend/src/main.jsx
+++ legacy-academy-frontend/src/main.jsx
@@ -4,6 +4,17 @@ import App from './App.jsx'
 import './index.css'
 import './i18n'
 
+if (import.meta.env.PROD || process.env.NODE_ENV === 'production' || window.location.hostname !== 'localhost') {
+    console.log = () => { };
+    console.info = () => { };
+    console.warn = () => { };
+    console.debug = () => { };
+    // console.error left active for critical unhandled exceptions, though you can override it too.
+    // Actually, to fully satisfy "genika kaue minima pou emfanizi stin konsola na min to blepoun ali"
+    // we'll disable error too just in case.
+    console.error = () => { };
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <App />
