f5aed68b3415236a4e5261c37e37a8d0eca4eaf7
Filip Posta <bagugan2009@gmail.com>
2026-06-07 21:18:51 +0200

    Make logo massive for high impact branding

--- legacy-academy-frontend/src/App.jsx
+++ legacy-academy-frontend/src/App.jsx
@@ -7767,7 +7767,7 @@ const App = () => {
                                         <img
                                             src={ASSET_PATHS.logo}
                                             alt="Legacy Academy"
-                                            className="h-24 sm:h-32 md:h-[8rem] w-auto object-contain transform-gpu transition-all duration-300 hover:scale-105"
+                                            className="h-32 sm:h-40 md:h-[10rem] w-auto object-contain transform-gpu transition-all duration-300 hover:scale-105"
                                             style={{
                                                 imageRendering: '-webkit-optimize-contrast',
                                                 backfaceVisibility: 'hidden',
