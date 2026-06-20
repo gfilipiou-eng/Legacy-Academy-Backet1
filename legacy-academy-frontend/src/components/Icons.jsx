f214018f6b710d293569546b7c9186e22d571af8
Filip Posta <bagugan2009@gmail.com>
2026-06-20 21:27:03 +0200

    Fix React Minified Error #130 by exporting LayoutTemplate icon

--- legacy-academy-frontend/src/components/Icons.jsx
+++ legacy-academy-frontend/src/components/Icons.jsx
@@ -129,4 +129,5 @@ export const Icons = {
             </circle>
         </svg>
     ),
+    LayoutTemplate: p => <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...p}><rect width="18" height="18" x="3" y="3" rx="2" /><path d="M3 9h18" /><path d="M9 21V9" /></svg>,
 };
